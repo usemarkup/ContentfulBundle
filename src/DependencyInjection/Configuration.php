a4d37312864471e46f6f76fd05e2eed8795047b6
Douglas Greenshields <douglas@usemarkup.com>
2014-06-19 17:09:19 +0200

    fix bundle configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Markup\ContentfulBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
@@ -14,7 +15,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('elnur_blowfish_password_encoder');
+        $rootNode = $treeBuilder->root('markup_contentful');
 
         $rootNode
             ->addDefaultsIfNotSet()
