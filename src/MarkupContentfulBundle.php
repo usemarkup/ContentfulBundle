9c945b3c7191e49f923e5591907572850c5ff45e
Douglas Greenshields <douglas@usemarkup.com>
2018-06-08 10:27:55 +0100

    declare console commands as services to enable lazy-loading if available

--- src/MarkupContentfulBundle.php
+++ src/MarkupContentfulBundle.php
@@ -2,7 +2,13 @@
 
 namespace Markup\ContentfulBundle;
 
+use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MarkupContentfulBundle extends Bundle
-{}
+{
+    public function registerCommands(Application $application)
+    {
+        //don't register commands through the bundle as they are registered as services
+    }
+}
